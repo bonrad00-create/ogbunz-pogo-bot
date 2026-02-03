46d1a0502f1fff1453da9c9e8b2d24370f2749a9
bonrad00-create <bonrad00@gmail.com>
2026-02-03 14:27:09 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -154,3 +154,11 @@ async function safeTick() {
 }
 
 client.login(DISCORD_TOKEN);
+
+const http = require("http");
+
+const port = process.env.PORT || 8080;
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("ok");
+}).listen(port, () => console.log("health server listening on", port));
